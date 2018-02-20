ee987410e7c05bb7525561bcb7261e850ae932ea
Joseph Cengel <s011878@lm-18s011878.lmsd.org>
2018-02-20 14:37:15 -0500

    Organization of basic features

---
+++ Movieheads/Load & Landing Screens/LandingScreenController.swift
@@ -0,0 +1,9 @@
+//
+//  LandingScreen.swift
+//  Movieheads
+//
+//  Created by Joseph Cengel (student LM) on 2/20/18.
+//  Copyright © 2018 Joseph Cengel (student LM). All rights reserved.
+//
+
+import Foundation
