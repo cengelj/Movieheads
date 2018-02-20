a303d734f0e6c2ab426e83e7e1ee395fbe07736e
Joseph Cengel <s011878@lm-18s011878.lmsd.org>
2018-02-20 14:28:04 -0500

    Started setup for movie controller

--- Movieheads/MovieController.swift
+++ Movieheads/MovieController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class MovieController: UIViewController {
-	
+	@IBOutlet var categories: [UILabel]!
+	@IBOutlet var movieImage: UIImage!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
