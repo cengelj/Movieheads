b812e7ed402974dd98557496ee40a2d818faaf84
Joseph Cengel <s011878@lm-18s011878.lmsd.org>
2018-02-20 14:16:57 -0500

    Created basic movie page and its view controller

--- Movieheads/MovieController.swift
+++ Movieheads/MovieController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  MovieController.swift
 //  Movieheads
 //
-//  Created by Joseph Cengel (student LM) on 2/14/18.
+//  Created by Joseph Cengel (student LM) on 2/20/18.
 //  Copyright © 2018 Joseph Cengel (student LM). All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MovieController: UIViewController {
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
 }
-
