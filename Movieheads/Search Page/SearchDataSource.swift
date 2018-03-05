ce77d6bbf5b2be493095894b4f9819bb1b8df811
t80088 <t80088@gmail.com>
2018-03-05 11:46:08 -0500

    The rest of the files that somehow became unversioned

--- Movieheads/Search Page/SearchDataSource.swift
+++ Movieheads/Search Page/SearchDataSource.swift
@@ -7,7 +7,37 @@
 //
 
 import UIKit
+import Pods_Movieheads
+import TMDBSwift
 
-class SearchDataSourch {
-    
+class SearchDataSource: NSObject, UITableViewDataSource {
+	var results = [MovieMDB]()
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return results.count
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		var cell:UITableViewCell
+		cell = tableView.dequeueReusableCell(withIdentifier: "Listing")!
+		let label = cell.viewWithTag(1) as! UILabel
+		let date = results[indexPath.row].release_date!.prefix(4)
+		label.text = "\(String(describing: results[indexPath.row].title!)) (\(date))"
+		
+		return cell
+	}
+	// To-Do: Limit user to 1 search every 10 seconds?
+	func filter(searchText: String, tableView: UITableView){
+		// Load Movie Names From API
+		SearchMDB.movie(query: searchText, language: "en", page: 1, includeAdult: true, year: nil, primaryReleaseYear: nil) { (data, m) in
+			if let movies = m{
+				self.set(movies: movies)
+				tableView.reloadData()
+			}
+		}
+	}
+	func set(movies:[MovieMDB]){
+		results = movies
+	}
+	func getResult(index:Int) -> MovieMDB{
+		return results[index]
+	}
 }
